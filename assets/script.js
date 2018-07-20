8947e579f3db546f0189e5cde87f449842c3be2a
LENOVO-PC\Urszula <ugrudzien12@gmail.com>
2018-07-20 19:47:58 +0200

    adding creatingHtmlList function

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,51 @@
-// import * as filestack from 'filestack-js';
+//  import * as filestack from '/node_modules/filestack-js';
 // const client = filestack.init('apikey');
 
 const client = filestack.init('ADY3Owf7wRiWAXEIJ7cCrz');
 
-    window.addEventListener('DOMContentLoaded', function () {
-    const apikey = 'ADY3Owf7wRiWAXEIJ7cCrz';
-    const client = filestack.init(apikey);
+window.addEventListener('DOMContentLoaded', function () {
+
     const options = {
         displayMode: 'inline',
         container: '#inline',
         maxFiles: 5,
         uploadInBackground: false,
+        onClose: function () {
+
+        },
         onUploadDone: (res) => console.log(res),
     };
     const picker = client.picker(options);
+    document.getElementById("content").innerHTML = picker;
+
     const openBtn = document.getElementById('open');
     const closeBtn = document.getElementById('close');
     openBtn.addEventListener('click', () => picker.open());
     closeBtn.addEventListener('click', () => picker.close());
 });
+
+function creatingHtmlList(textElement){
+    var ul = document.getElementById("filelist");
+    var li= document.createElement("li");
+    ul.appendChild(li);
+    var t=document.createTextNode(textElement);
+    li.innerHTML=li.innerHTML + textElement;
+}
+creatingHtmlList("ja");
+
+// document.querySelector('input').addEventListener('change', (event) => {
+//     const files = event.target.files;
+//     const file = files.item(0);
+//     const token = {};
+
+
+//     client.upload(file, {
+//             onProgress
+//         }, {}, token)
+//         .then(res => {
+//             console.log('success: ', res)
+//         })
+//         .catch(err => {
+//             console.log(err)
+//         });
+// });
