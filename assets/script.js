ae86fd4395e02a170957eec545cf17b06a5c7b90
LENOVO-PC\Urszula <ugrudzien12@gmail.com>
2018-07-20 12:54:16 +0200

    louder

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+// import * as filestack from 'filestack-js';
+// const client = filestack.init('apikey');
+
+const client = filestack.init('ADY3Owf7wRiWAXEIJ7cCrz');
+
+    window.addEventListener('DOMContentLoaded', function () {
+    const apikey = 'YOUR_APIKEY';
+    const client = filestack.init(apikey);
+    const options = {
+        displayMode: 'inline',
+        container: '#inline',
+        maxFiles: 20,
+        uploadInBackground: false,
+        onUploadDone: (res) => console.log(res),
+    };
+    const picker = client.picker(options);
+    const openBtn = document.getElementById('open');
+    const closeBtn = document.getElementById('close');
+    openBtn.addEventListener('click', () => picker.open());
+    closeBtn.addEventListener('click', () => picker.close());
+});
