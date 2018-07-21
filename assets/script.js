e9764515679555e6f8a888fa2fafedd39827f425
LENOVO-PC\Urszula <ugrudzien12@gmail.com>
2018-07-21 23:18:14 +0200

    list element as a link open in new window

--- assets/script.js
+++ assets/script.js
@@ -4,37 +4,52 @@
 const client = filestack.init('ADY3Owf7wRiWAXEIJ7cCrz');
 
 window.addEventListener('DOMContentLoaded', function () {
-
+    var files = [];
     const options = {
         displayMode: 'inline',
         container: '#inline',
         maxFiles: 5,
         uploadInBackground: false,
+        disableThumbnails: false,
         onClose: function () {
 
         },
-        onUploadDone: creatingHtmlList,
+        onUploadDone: creatingHTMLList,
     };
     const picker = client.picker(options);
-
+ 
 
     const openBtn = document.getElementById('open');
     const closeBtn = document.getElementById('close');
     openBtn.addEventListener('click', () => picker.open());
     closeBtn.addEventListener('click', () => picker.close());
 });
-
+// const inputEl = document.getElementById('fileSelect');
+// const picker = client.picker({
+//   onUploadDone: res => console.log(res),
+// });
+// inputEl.addEventListener('change', (e) => {
+//   picker.crop(e.target.files);
+// });
+// files.push('');
 // helper function for creating an unorderer HTML list using javascript I took from https://stackoverflow.com/questions/11351135/create-ul-and-li-elements-in-javascript
-function creatingHtmlList(response) {
+
+function creatingHTMLList(response) {
      var pickerFileMetadata = response.filesUploaded; //table of pickerFileMetadata object
     for (var i = 0; i < pickerFileMetadata.length; i++) {
         var filename = pickerFileMetadata[i].filename; //taking a filename properties from pickerfilemetadata object
+        var fileUrl = pickerFileMetadata[i].url;
         var ul = document.getElementById("filelist");
         var li = document.createElement("li");
         ul.appendChild(li);
-        var t = document.createTextNode(filename);
-        li.innerHTML = li.innerHTML + filename;
+        var a=document.createElement("a");
+        a.setAttribute("href",fileUrl);
+        a.setAttribute("target", "_blank");
+        li.appendChild(a);
+        // var t = document.createTextNode(filename);
+        a.innerHTML = a.innerHTML + filename;
     }
 }
+
 
 
