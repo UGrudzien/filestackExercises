b3c4ff323e0cf04bd1ea330e15e12d0feb1cf30e
LENOVO-PC\Urszula <ugrudzien12@gmail.com>
2018-07-23 10:42:33 +0200

    rebuilding creatingHTMLList function

--- assets/script.js
+++ assets/script.js
@@ -14,7 +14,7 @@ window.addEventListener('DOMContentLoaded', function () {
         onClose: function () {
 
         },
-        onUploadDone: creatingHTMLList,
+        onUploadDone: onUpload,
 
     };
     const picker = client.picker(options);
@@ -25,60 +25,59 @@ window.addEventListener('DOMContentLoaded', function () {
     openBtn.addEventListener('click', () => picker.open());
     closeBtn.addEventListener('click', () => picker.close());
 });
-// const inputEl = document.getElementById('fileSelect');
-// const picker = client.picker({
-//   onUploadDone: res => console.log(res),
-// });
-// inputEl.addEventListener('change', (e) => {
-//   picker.crop(e.target.files);
-// });
-// files.push('');
+
 // helper function for creating an unorderer HTML list using javascript I took from https://stackoverflow.com/questions/11351135/create-ul-and-li-elements-in-javascript
 
-function creatingHTMLList(response) {
-    var pickerFileMetadata = response.filesUploaded; //table of pickerFileMetadata object
+function onUpload(response) {
+    var pickerFileMetadata = response.filesUploaded; //table of pickerFileMetadata array
 
     for (var i = 0; i < pickerFileMetadata.length; i++) {
         var filename = pickerFileMetadata[i].filename; //taking a filename properties from pickerfilemetadata object
         var fileUrl = pickerFileMetadata[i].url;
-        var clientTransform = client.transform(fileUrl, {
-            resize: {
-                height: 50,
-              width: 50,
-            },
-        })
-        var ul = document.getElementById("filelist");
-        var li = document.createElement("li");
-        ul.appendChild(li);
-        var a = document.createElement("a");
-        a.setAttribute("href", fileUrl);
-        a.setAttribute("target", "_blank");
-        li.appendChild(a);
-        const img = document.createElement('img');
-        img.src= clientTransform;
-      
-        var div = document.createElement("div");
-       
-        a.appendChild(div);
-          div.appendChild(img);
-          var span =  document.createElement('span');
-           a.appendChild(span);
-           span.innerText=filename;
-           
-    //     client.retrieve('9v04TW7ZQLi9V2V3InV0').then((blob) => {
-    //         const urlCreator = window.URL || window.webkitURL;
-    //         const img = document.createElement('img');
-    //         img.width = 720;
-    //         img.height = 480;
-    //         img.src = urlCreator.createObjectURL(blob);
-
-    //         document.getElementById('content').appendChild(img)
-    //     }).catch((error) => {
-    //         console.error(error);
-    //     });
-    // });
-// var t = document.createTextNode(filename);
-//span.innerHTML = span.innerHTML+filename;
-a.innerHTML = a.innerHTML ;
+        var imageTransform = imageUrlThunbnailTransformation(fileUrl); //url for thumnail image
+        creatingHTMLList(fileUrl, filename, imageTransform);
+        document.getElementsByTagName("span").innerText = filename;
+        imageThumbnail(ImageTransform);
+    }
 }
+
+function imageUrlThunbnailTransformation(fileUrl) { //changing original image into the thumnail image
+    var ImageTransform = client.transform(fileUrl, {
+        resize: {
+            height: 50,
+            width: 50,
+        },
+    })
+    return ImageTransform;
+}
+function documentUrlThumbnailTransformation(){
+    
+}
+
+function creatingHTMLList(fileUrl, filename, imageTransform) {
+    var ul = document.getElementById("filelist");
+    var li = document.createElement("li");
+    ul.appendChild(li);
+
+    var a = document.createElement("a");
+    a.setAttribute("href", fileUrl);
+    a.setAttribute("target", "_blank");
+    li.appendChild(a);
+
+    var div = document.createElement("div");
+    a.appendChild(div);
+        const img = document.createElement('img');
+    img.src = imageTransform;
+    // var div = document.createElement("div");
+    div.appendChild(img);
+ 
+    var span = document.createElement('span');
+    a.appendChild(span);
+    span.innerText = filename;
+
+    a.innerHTML = a.innerHTML;
+}
+
+function imageThumbnail(imageTransform) { //creating a img HTML element
+
 }
