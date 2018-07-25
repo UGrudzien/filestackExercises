bf5cdfbc044d46ffa10eedd41f50b2966081fba3
LENOVO-PC\Urszula <ugrudzien12@gmail.com>
2018-07-25 18:48:34 +0200

    server working with js and css

--- server.js
+++ server.js
@@ -1,16 +1,46 @@
+//source of belowed code http://ericsowell.com/blog/2011/5/6/serving-static-files-from-node-js
+
+
 var http = require('http');
-
-var fs = require("fs")
-
-http.createServer(function (req, res) {
-
-    
-    fs.readFile("./index.html", function (err, data) {
-        res.writeHead(200, {
-            'Content-Type': 'text/html'
-        });
-        res.write(data, "utf-8");
-        return res.end();
-    })
-
-}).listen(8080);
+var fs = require('fs');
+var path = require('path');
+http.createServer(function (request, response) {
+    console.log('request starting...');
+	
+	var filePath = '.' + request.url;
+	if (filePath == './')
+		filePath = './index.html';
+		
+	var extname = path.extname(filePath);
+	var contentType = 'text/html';
+	switch (extname) {
+		case '.js':
+			contentType = 'text/javascript';
+			break;
+		case '.css':
+			contentType = 'text/css';
+			break;
+	}
+	
+	fs.exists(filePath, function(exists) {
+	
+		if (exists) {
+			fs.readFile(filePath, function(error, content) {
+				if (error) {
+					response.writeHead(500);
+					response.end();
+				}
+				else {
+					response.writeHead(200, { 'Content-Type': contentType });
+					response.end(content, 'utf-8');
+				}
+			});
+		}
+		else {
+			response.writeHead(404);
+			response.end();
+		}
+	});
+	
+}).listen(8125);
+console.log('Server running at http://127.0.0.1:8125/');
