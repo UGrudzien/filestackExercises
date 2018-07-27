9c95d391e3ba531265659e0cbdc2cee55e01277f
LENOVO-PC\Urszula <ugrudzien12@gmail.com>
2018-07-27 10:07:47 +0200

    correcting readme

--- server.js
+++ server.js
@@ -5,13 +5,15 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 const sqlite3 = require('sqlite3').verbose();
-	
+
 let db = new sqlite3.Database('data.db', (err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Connected to the in-memory SQlite database.');
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite database.');
 });
+ 
+
 
 
 http.createServer(function (request, response) {
@@ -23,7 +25,7 @@ http.createServer(function (request, response) {
 
     var extname = path.extname(filePath); //The path.extname() method returns the extension of the path, from the last occurrence of the . (period) character to end of string in the last portion of the path
     var contentType = 'text/html';
- 
+
     //Using fs.exists() to check for the existence of a file before calling fs.open(), fs.readFile() or fs.writeFile() is not recommended. Doing so introduces a race condition, since other processes may change the file's state between the two calls. Instead, user code should open/read/write the file directly and handle the error raised if the file does not exist.   
 
     fs.readFile(filePath, function (error, content) {
@@ -37,14 +39,38 @@ http.createServer(function (request, response) {
             response.end(content, 'utf-8');
         }
     });
-    
 
-   
+
+
 
 
 }).listen(8080);
 console.log('Server running at http://127.0.0.1:8080/');
 // var mysql = require('mysql');
-
-
+function run(sql, params = []) {
+    return new Promise((resolve, reject) => {
+      this.db.run(sql, params, function (err) {
+        if (err) {
+          console.log('Error running sql ' + sql)
+          console.log(err)
+          reject(err)
+        } else {
+          resolve({ id: this.lastID })
+        }
+      })
+    })
+  }
+function createTable() {
+    const sql = `
+    CREATE TABLE IF NOT EXISTS db (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT
+      urt TEXT)`
+    return db.run(sql)
+  }
+  function  create(name) {
+    return dbo.run(
+      'INSERT INTO db (name) VALUES (?)',
+      [name])
+  }
 //db.close();
