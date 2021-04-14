6ba6d5e1ce449e360f0d0bf9edea73f1be9c4011
urszula <ugrudzien12@gmail.com>
2021-04-14 08:55:38 +0200

    add crop function

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,24 @@
 window.addEventListener('DOMContentLoaded', function () {
   const apikey = 'A2Xx0v3NRZiCOIQk275hAz';
   const client = filestack.init(apikey);
+  var originalURL = "https://cdn.filestackcontent.com/PFqwRdbRuqrYyAH5pOaI";
+
   const options = {
       displayMode: 'inline',
       container: '#inline',
       maxFiles: 20,
      
       uploadInBackground: false,
-      onUploadDone: (res) => console.log(res),
+      onUploadDone: function (result) {
+        console.log('done', result);
+      },
+      onFileUploadFailed: function (err) {
+        console.log('failed', err);
+      },
+      // onUploadDone: (res) => console.log(res),
   };
   const picker = client.picker(options);
+  picker.crop(originalURL)
   const openBtn = document.getElementById('open');
   const closeBtn = document.getElementById('close');
   openBtn.addEventListener('click', () => picker.open());
